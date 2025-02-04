99037c73371e97f1d91ff69646b258a9863d7862
mfsrajput <mfs.khi@gmail.com>
2025-02-04 16:37:37 +0500

    moved Figma-hacthoon one repo to another for vercel deplyment

---
+++ figma-hackhathoon/src/app/page.tsx
@@ -0,0 +1,25 @@
+// import { HeroSection } from "@/components/HeroSection";
+// import { BrowseRange } from "@/components/BrowseRange";
+// import  OurProduct  from "@/components/OurProduct";
+// import  Inspiration  from "@/components/Inspiration";
+// import { ShareSection } from "@/components/ShareSection";
+// import Gallery from "@/components/Gallery";
+
+import Home from "./Home/page";
+
+
+export default function HomePage() {
+  return (
+    <div>
+      <Home/>
+      {/* <HeroSection />
+      <BrowseRange />
+      <OurProduct />
+      <Inspiration />
+      <Gallery />
+      <ShareSection /> */}
+      
+
+    </div>
+  );
+}
